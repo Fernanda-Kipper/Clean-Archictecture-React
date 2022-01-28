cc943a38c786d04170bc3f59d04fce4a2869254b
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-01-28 17:22:57 -0300

    fix: lint jest config json

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '<rootDir>/src/*.{ts,tsx}',
+    '<rootDir>/src/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
