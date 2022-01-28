e70d2f96fad0c6c3444a87931c00505696d6c712
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-01-28 17:09:31 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/src/*.{ts,tsx}',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  }
+}
