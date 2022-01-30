0d49d4cfec1af0030d2db07c55082049f7d3aee5
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-01-30 11:34:56 -0300

    test: ensure RemoteAuthentication calls HttpPostClient

---
+++ src/data/use-cases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,22 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+describe('Remote Authentication', () => {
+  test('Should call httpPostClient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string
+
+      async post (url: string): Promise<void> {
+        this.url = url
+        return await Promise.resolve()
+      }
+    }
+
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
