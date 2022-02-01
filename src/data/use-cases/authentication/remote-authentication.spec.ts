f520450885760fca4a245458849ee210673389a5
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-01 17:11:43 -0300

    refactor: group imports in index files

--- src/data/use-cases/authentication/remote-authentication.spec.ts
+++ src/data/use-cases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,13 @@
 import faker from 'faker'
 
-import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { RemoteAuthentication } from './remote-authentication'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { AuthenticationParams } from '@/domain/use-cases/authentication'
-import { AccountModel } from '@/domain/models/account-model'
+
+import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
+import { HttpPostClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AuthenticationParams } from '@/domain/use-cases'
+import { AccountModel } from '@/domain/models'
 
 type SutTypes = {
   sut: RemoteAuthentication
--- src/data/use-cases/authentication/remote-authentication.ts
+++ src/data/use-cases/authentication/remote-authentication.ts
@@ -1,9 +1,7 @@
-import { HttpPostClient } from '@/data/protocols/http/http-post-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { AccountModel } from '@/domain/models/account-model'
-import { Authentication, AuthenticationParams } from '@/domain/use-cases/authentication'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
+import { Authentication, AuthenticationParams } from '@/domain/use-cases'
 
 export class RemoteAuthentication implements Authentication {
   constructor (
