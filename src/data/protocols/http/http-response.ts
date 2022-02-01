87ba08ab9fa5bcee05e2d589bfca06768404c676
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-01 16:52:40 -0300

    feat: ensure RemoteAuth throws unexpected error on default cases

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,6 +1,10 @@
 export enum HttpStatusCode {
+  ok = 200,
+  noContent = 204,
   unauthorized = 401,
-  noContent = 204
+  badRequest = 400,
+  notFound = 404,
+  internalServerError = 500,
 }
 
 export type HttpResponse = {
--- src/data/use-cases/authentication/remote-authentication.ts
+++ src/data/use-cases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AuthenticationParams } from '@/domain/use-cases/authentication'
 
 export class RemoteAuthentication {
@@ -13,10 +14,9 @@ export class RemoteAuthentication {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.unauthorized: {
-        throw new InvalidCredentialsError()
-      }
-      default: return await Promise.resolve()
+      case HttpStatusCode.ok: break
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
