358f92c3e28b9df69f2f7c52af44d95ab9c1074c
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-02 09:48:12 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '<rootDir>/src/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
