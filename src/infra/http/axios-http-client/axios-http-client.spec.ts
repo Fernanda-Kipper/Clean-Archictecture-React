814eb07c3c0d90f4c01a8439721dfe4a56cc4211
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-01 23:40:53 -0300

    refactor: organized imports

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
-import { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 import faker from 'faker'
 
+import { HttpPostParams } from '@/data/protocols/http'
 import { AxiosHttpClient } from './axios-http-client'
 
 const mockedAxiosResult = { status: faker.datatype.number(), data: faker.random.objectElement() }
