d974fa0f8cc71b0791308237f0cea0ff5ff57b30
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-01 23:32:53 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -17,12 +17,12 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and method', async () => {
+  test('Should call axios with correct values', async () => {
     const request = mockPostRequest()
     const sut = makeSut()
 
-    await sut.post({ url: request.url })
+    await sut.post(request)
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
