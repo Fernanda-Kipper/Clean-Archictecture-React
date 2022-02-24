49c2ef798286c97cabc6747747c8de1c4a055314
Fernanda Kipper <nanda.kipper@gmail.com>
2022-02-24 20:26:12 -0300

    feat: add form context

--- src/presentation/components/formStatus/form-status.tsx
+++ src/presentation/components/formStatus/form-status.tsx
@@ -1,17 +1,14 @@
-import React, { ReactElement } from 'react'
+import { Context } from '@/presentation/contexts/form-context'
+import React, { ReactElement, useContext } from 'react'
 import { Spinner } from '../spinner/spinner'
 import Styles from './form-status-styles.scss'
 
-type Props = {
-  isLoading?: boolean
-  error?: string
-}
-
-export function FormStatus (props: Props): ReactElement {
+export function FormStatus (): ReactElement {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={Styles.errorWrapper}>
-        {props.isLoading ? <Spinner /> : <></>}
-        {props.error ?? <span className={Styles.error}>{props.error}</span>}
+    <div className={Styles.errorWrapper} data-testid="error-wrapper">
+        {isLoading && <Spinner />}
+        {errorMessage ?? <span className={Styles.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,23 +1,36 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 
 import LoginHeader from '@/presentation/components/header/login-header'
 import Styles from './login-styles.scss'
 import { Footer } from '@/presentation/components/footer/footer'
 import { Input } from '@/presentation/components/input/input'
 import { FormStatus } from '@/presentation/components/formStatus/form-status'
+import { Context } from '@/presentation/contexts/form-context'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 function Login (): ReactElement {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
       <div className={Styles.login}>
           <LoginHeader />
-          <form className={Styles.form}>
-            <h2>Login</h2>
-            <Input type="email" name="email" placeholder="Digite seu e-mail"/>
-            <Input type="password" name="password" placeholder="Digite sua senha"/>
-            <button type="submit">Enviar</button>
-            <span className={Styles.link}>Criar conta</span>
-            <FormStatus isLoading error="Erro inesperado"/>
-          </form>
+          <Context.Provider value={state}>
+            <form className={Styles.form}>
+              <h2>Login</h2>
+              <Input type="email" name="email" placeholder="Digite seu e-mail"/>
+              <Input type="password" name="password" placeholder="Digite sua senha"/>
+              <button type="submit">Enviar</button>
+              <span className={Styles.link}>Criar conta</span>
+              <FormStatus />
+            </form>
+          </Context.Provider>
           <Footer />
       </div>
   )
