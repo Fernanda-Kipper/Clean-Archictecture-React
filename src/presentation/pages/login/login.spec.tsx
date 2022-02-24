ac479ecd45adc8d85cb3635c7f22d6b05fbe0e29
Fernanda Kipper <nanda.kipper@gmail.com>
2022-02-24 20:48:04 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { Login } from '..'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+
+  return {
+    sut
+  }
+}
 
 describe('Login Page', () => {
   test('Should initial render should be  correct initial state', () => {
-    const { getByTestId } = render(<Login />)
+    const { sut } = makeSut()
 
-    const emailStatus = getByTestId('email-status')
-    const passwordStatus = getByTestId('email-status')
+    const emailStatus = sut.getByTestId('email-status')
+    const passwordStatus = sut.getByTestId('email-status')
 
-    expect(getByTestId('error-wrapper').childElementCount).toBe(0)
-    expect((getByTestId('submit') as HTMLButtonElement).disabled).toBe(true)
+    expect(sut.getByTestId('error-wrapper').childElementCount).toBe(0)
+    expect((sut.getByTestId('submit') as HTMLButtonElement).disabled).toBe(true)
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
     expect(passwordStatus.title).toBe('Campo obrigatório')
