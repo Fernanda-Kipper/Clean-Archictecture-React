c7aa9df2758d6f3f6db69e361ee7399c23c01d8e
Fernanda Kipper <nanda.kipper@gmail.com>
2022-02-24 22:06:50 -0300

    refactor: use faker to generate random values on login test

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,6 +3,7 @@ import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react
 import { Login } from '..'
 import { Validation } from '@/presentation/protocols/validation'
 import { ValidationSpy } from '@/presentation/test/mock-validation'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RenderResult
@@ -40,21 +41,23 @@ describe('Login Page', () => {
 
   test('Should call validation with correct email', () => {
     const { sut, validationSpy } = makeSut()
+    const email = faker.internet.email()
 
     const emailInput = sut.getByTestId('email')
-    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    fireEvent.input(emailInput, { target: { value: email } })
 
     expect(validationSpy.fieldName).toEqual('email')
-    expect(validationSpy.fieldValue).toEqual('any_email')
+    expect(validationSpy.fieldValue).toEqual(email)
   })
 
   test('Should call validation with correct password', () => {
     const { sut, validationSpy } = makeSut()
+    const password = faker.internet.email()
 
     const passwordInput = sut.getByTestId('password')
-    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    fireEvent.input(passwordInput, { target: { value: password } })
 
     expect(validationSpy.fieldName).toEqual('password')
-    expect(validationSpy.fieldValue).toEqual('any_password')
+    expect(validationSpy.fieldValue).toEqual(password)
   })
 })
